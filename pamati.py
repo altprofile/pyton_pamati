8a4dac0f685f0f7c1366954ff821ad8a30877030
Karlis Lejnieks <karlisalbe@gamil.com>
2023-08-09 18:02:43 +0300

    majasdarbs

--- pamati.py
+++ pamati.py
@@ -6,7 +6,8 @@ c = 3
 for i in range(c):
   a = int(input("sk 1 - 10: "))
   if a == b:
-    print("yay")
+    print("ir")
+    break
   else:
     c = c -1
     print("nav bet tev ir", c, "meginajumi")
